1a0b9b4ad816796774c212592abc88e484248ecd
salvoab <salvatore.abate85@gmail.com>
2021-02-01 14:01:45 +0100

    Istruzioni dell'esercizio come commento e stile css in tag style

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Riscrivere questa pagina del sito google
+https://policies.google.com/faq. 
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google FAQ</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body{
+            font-family: sans-serif;
+        }
+
+        h2{
+            font-size: 24px;
+            font-weight: 500;
+            line-height: 32px;
+        }
+
+        p{
+            padding-top: 20px;
+            font-size: 14px;
+            line-height: 24px;
+        }
+
+        .container {
+            width: 80%;
+            margin: auto;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
